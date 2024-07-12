538fec44eae610a1a18f7bbee7ed85fb4d8ac42e
Dante Dullka <dullkaconst@gmail.com>
2024-07-12 15:02:15 -0300

    se quita modules

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql');
+const config = require('./config.js');  // Importa el archivo config.js
 
+// Crea la conexión a la base de datos
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'LimoneroDelFondo',
-  database: 'pelicula_db'
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  port: config.DB_PORT
 });
+
 
 db.connect((err) => {
   if (err) {
