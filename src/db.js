00af67c935effa16f2a7a418f3ef15c54dbd3499
Dante Dullka <dullkaconst@gmail.com>
2024-07-12 13:13:11 -0300

    cambios

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'LimoneroDelFondo4281#',
+  password: 'LimoneroDelFondo',
   database: 'pelicula_db'
 });
 
