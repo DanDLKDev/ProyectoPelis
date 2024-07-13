81c1908b1229bbbde1580895f77b461c46f42ac2
Dante Dullka <dullkaconst@gmail.com>
2024-07-13 16:37:45 -0300

    arreglo6

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-
+require('dotenv').config();
 module.exports = {
-    PORT: process.env.PORT || 3000,
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'LimoneroDelFondo',
-    DB_NAME: process.env.DB_NAME || 'pelicula_db',
-    DB_PORT: process.env.DB_PORT || 3306,
+    PORT: process.env.PORT, 
+    DB_HOST: process.env.DB_HOST, 
+    DB_USER: process.env.DB_USER, 
+    DB_PASSWORD: process.env.DB_PASSWORD, 
+    DB_NAME: process.env.DB_NAME, 
+    DB_PORT: process.env.DB_PORT,
   };
