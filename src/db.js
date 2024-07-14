2c17c5ce6e3a60fc7b4ae8fe9beb3aabe3a808ba
Dante Dullka <dullkaconst@gmail.com>
2024-07-14 11:02:19 -0300

    ahora si?

--- src/db.js
+++ src/db.js
@@ -1,26 +1,4 @@
-/*const mysql = require('mysql');
-const config = require('./config.js');  // Importa el archivo config.js
 
-// Crea la conexión a la base de datos
-const db = mysql.createConnection({
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
-  database: config.DB_NAME,
-  port: config.DB_PORT
-});
-
-
-db.connect((err) => {
-  if (err) {
-    console.error('Error conectando a MySQL:', err.stack);
-    return;
-  }
-  console.log('Conectado a MySQL como ID:', db.threadId);
-});
-
-module.exports = db;
-*/
 
 
 const mysql = require('mysql2');
