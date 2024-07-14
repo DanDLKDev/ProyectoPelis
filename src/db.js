b13513b08f36802906940468ae1c17f4dfeb5454
Dante Dullka <dullkaconst@gmail.com>
2024-07-14 10:40:09 -0300

    arreglofinal?

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+/*const mysql = require('mysql');
 const config = require('./config.js');  // Importa el archivo config.js
 
 // Crea la conexión a la base de datos
@@ -19,4 +19,32 @@ db.connect((err) => {
   console.log('Conectado a MySQL como ID:', db.threadId);
 });
 
+module.exports = db;
+*/
+
+
+const mysql = require('mysql2');
+const config = require('./config');  // Asegúrate de que la ruta sea correcta
+
+// Crea el pool de conexiones
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  port: config.DB_PORT
+});
+
+// Verificar conexión
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error al conectar con la base de datos:', err.stack);
+    return;
+  }
+  console.log('Conectado a la base de datos como ID ' + connection.threadId);
+  connection.release();
+});
+
+// Exporta el pool de conexiones
 module.exports = db;
